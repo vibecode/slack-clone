f15664a50b1d5fca402bd5f44132cfb34d67a5d3
vibecode <rumashkin@gmail.com>
2018-12-07 18:33:49 +0100

    setup apollo server

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-import { eslint } from 'eslint'
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+import typeDefs from './schema'
+import resolvers from './resolvers'
 
-const = 
+const PORT = 5000
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+const server = new ApolloServer({ schema })
+
+const app = express()
+server.applyMiddleware({ app })
+
+app.listen(PORT, () =>
+  //eslint-disable-next-line
+  console.log(
+    `🚀  Server is listening at http://localhost:${PORT}${server.graphqlPath}`
+  )
+)
